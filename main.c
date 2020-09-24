616e741a01842b6193594b8dbb6b9c9fe711e563
Ahmed Saber <ahmedfadlahmed24@gmail.com>
2020-09-24 14:28:05 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    printf ("My first name is : ahmed\n");
+    printf ("My age is : 19\n");
+    printf ("My tall is :182\n");
+    printf ("My weight is :82\n");
+    printf ("My faculty is: facult of electronic engineering");
+
+    return 0;
+}
